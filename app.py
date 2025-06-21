3085cb5a57208171f0a0f1969c8c8edfe5d4e78c
Mateus-Sebastião <mateussebastiao2003@gmail.com>
2025-06-21 22:40:16 +0100

    Update app.py for using Flask

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
-print('Hello world')
-print('I\'m Mateus')
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home:
+    return "Hello world"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=true)
